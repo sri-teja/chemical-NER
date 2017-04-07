ba0cae7ac1a654af31eeed1f3749e131741693f3
sri-teja <asriteja77712@gmail.com>
2017-04-07 18:17:08 +0530

    training with new features

--- features.py
+++ features.py
@@ -134,6 +134,11 @@ ELEMENTS = {
     117: 'us',
     118: 'uo',
 }
+
+with open('chemtionary.txt') as f:
+	content = f.readlines()
+
+chemtionary = content 
 print "*********** Creating the features for each word **********"
 def get_features(str1):
 	u = 0
@@ -187,8 +192,13 @@ def get_features(str1):
 		
 		if i in exclude and pun==0:
 		 	pun = 1
+
+		if ''.join(e for e in str1 if e.isalpha()) in chemtionary:
+			pid = 1
+		else:
+			pid = 0
 	if valid and len(case_pattern):			
-		return u, l, c, pun, case_pattern, n_gram, molecular_score
+		return u, l, c, pun, case_pattern, n_gram, molecular_score, pid
 
 features = {}
 for i in word_n_grams:
@@ -206,10 +216,10 @@ for i in word_n_grams:
 		
 
 extract_file_name = str(''.join((sys.argv[1]).split('/')[0:-1])) + '/' + str(sys.argv[1]).split('/')[-1:][0].split('.')[0]+"_features.csv"
-f= open("inputtext/testfile.txt","wa")
+f= open("inputtext/trainfile.txt","wa")
 
 for key,val in features.items():
-	f.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(key.encode('utf-8'),val[0],val[1],val[2],val[3],val[4],val[5].encode('utf-8'),val[6]))
+	f.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(key.encode('utf-8'),val[0],val[1],val[2],val[3],val[4],val[5].encode('utf-8'),val[6], val[7], val[8]))
 
 #import pickle
 #pickle.dump(features, f)
